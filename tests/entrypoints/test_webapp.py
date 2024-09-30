65564edbab75d30de47499cc87b91260b7885906
angelo-volpe <angelovolpe95@gmail.com>
2024-09-30 22:32:58 +0200

    add tests for wrong api input, remove unused param model

--- tests/entrypoints/test_webapp.py
+++ tests/entrypoints/test_webapp.py
@@ -8,10 +8,7 @@ def test_object_detection(client, image_path):
         image_data = f.read()
     image = io.BytesIO(image_data)
     
-    data = {
-        'threshold': '0.9',
-        'model_name': 'rfcn',
-    }
+    data = {'threshold': '0.9'}
     data['file'] = (image, 'test.jpg')
 
     # Make a test request to the object_detection endpoint
@@ -22,7 +19,48 @@ def test_object_detection(client, image_path):
     # and status code is correct(Integration test)
     assert response.status_code == 200
     assert json.loads(response.data) != None
+    
+def test_object_detection_wrong_number_threshold(client, image_path):
+    # Load the image from the path resource/boy.jpg
+    with open(image_path, 'rb') as f:
+        image_data = f.read()
+    image = io.BytesIO(image_data)
 
+    data = {'threshold': '10'}
+    data['file'] = (image, 'test.jpg')
+
+    # Make a test request to the object_detection endpoint
+    response = client.post('/object-count', data = data,
+        content_type='multipart/form-data', buffered=True)
+
+    assert response.status_code == 400
+
+def test_object_detection_string_threshold(client, image_path):
+    # Load the image from the path resource/boy.jpg
+    with open(image_path, 'rb') as f:
+        image_data = f.read()
+    image = io.BytesIO(image_data)
+
+    data = {'threshold': 'a'}
+    data['file'] = (image, 'test.jpg')
+
+    # Make a test request to the object_detection endpoint
+    response = client.post('/object-count', data = data,
+        content_type='multipart/form-data', buffered=True)
+
+    assert response.status_code == 400
+
+def test_object_detection_wrong_file(client):
+    image = io.BytesIO()
+
+    data = {'threshold': '10'}
+    data['file'] = (image, 'test.txt')
+
+    # Make a test request to the object_detection endpoint
+    response = client.post('/object-count', data = data,
+        content_type='multipart/form-data', buffered=True)
+
+    assert response.status_code == 400
 
 def test_predictions_list(client, image_path):
     # Load the image from the path resource/boy.jpg
@@ -30,10 +68,7 @@ def test_predictions_list(client, image_path):
         image_data = f.read()
     image = io.BytesIO(image_data)
     
-    data = {
-        'threshold': '0.9',
-        'model_name': 'rfcn',
-    }
+    data = {'threshold': '0.9'}
     data['file'] = (image, 'test.jpg')
 
     # Make a test request to the predictions-list endpoint
